78049f60a78480e0926daeefe07e0b735a1986d7
Svetlana Kutyova <kutyovas@yandex.ru>
2021-11-18 00:13:40 +0300

    1

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -4,18 +4,10 @@ import Magician from '../Magician.js';
 
 test('test error toThrow', () => {
   expect(() => {
-    const p1 = new Magician('hero', 'Magician');
   }).toThrow(/Персонаж уже входит в команду/);
 });
 
-test('test успешное добавление', () => {
-  const p = new Character('hero', 'Magician');
-  const p0 = new Daemon('hero1', 'Daemon');
-});
-
 test('test преобразование в массив', () => {
-  const p1 = new Character('hero', 'Magician');
-  const p2 = new Character('hero1', 'Bowman');
   /*
   expect(team.toArray()).toStrictEqual([...team.members]);
   */
