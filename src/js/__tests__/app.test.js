23113d4c434b8ebfe155b08743783f123434010f
Svetlana Kutyova <kutyovas@yandex.ru>
2021-11-26 19:18:21 +0300

    2

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -1,11 +1,8 @@
-import Daemon from '../Daemon.js';
+import Character from '../Character.js';
 import Magician from '../Magician.js';
 
-test('test преобразование в массив', () => {
-  /*
-  expect(team.toArray()).toStrictEqual([...team.members]);
-  */
-  const p2 = new Daemon('hero1', 'Daemon');
-  const p3 = new Magician('hero2', 'Magician');
-  console.log(p3, p2);
+test('обьекты ', () => {
+  const result1 = new Magician('hero2', 'Magician');
+  const result2 = new Character('hero2', 'Magician');
+  expect(result1).toMatchObject(result2);
 });
